afc4eb8b1e9d7ad3cb86e5e9a455c6d979d16a09
Charaka <charakaweb@gmail.com>
2026-01-02 01:56:19 +0530

    feat: Add decimal.js dependency

---
+++ src/app/domains/calculators/data/models.ts
@@ -0,0 +1,13 @@
+export interface FutureValueParams {
+    pv: number;
+    rate: number;
+    periods: number;
+}
+
+export interface PresentValueParams {
+    fv: number;
+    rate: number;
+    periods: number;
+}
+
+// Add more as needed during migration
